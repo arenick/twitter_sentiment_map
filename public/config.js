f7bfe34dc49735281702565741ddb50cebf7d270
John Permaloff <jvpermaloff@gmail.com>
2018-09-07 13:52:26 -0400

    rev.1

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
-angular
-    .module("App")
-    .config(["$routeProvider", function($routeProvider) {
-        $routeProvider
-            .when("/map", {
-                template: `<twitter-map></twitter-map>`
-            })
-    }
-])
+// angular
+//     .module("App")
+//     .config(["$routeProvider", function($routeProvider) {
+//         $routeProvider
+//             .when("/map", {
+//                 template: `<twitter-map></twitter-map>`
+//             })
+//     }
+// ])
