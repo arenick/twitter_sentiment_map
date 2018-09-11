10099531163489ca841c28ef4ccf92730f8ada85
Kenneth <rekerc4@gmail.com>
2018-09-11 11:46:29 -0400

    merging master

--- public/twitterService.js
+++ public/twitterService.js
@@ -1,14 +1,36 @@
 
 "use strict";
-function TwitterService ($http) {
+function TwitterService ($http, $sce) {
     const vm = this;
     vm.getAllTweets = () => {
         return $http({
             method: "GET",
-           url: "/search",
+           url: "/search/all",
                  
         }).then((response) => {
-            console.log(response.data);
+            let data = {}; 
+            let sentimentArray = [];
+            console.log(response);
+            let loop = (entry) => {
+                let urlReplace = entry.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
+                console.log(urlReplace);
+                let specialReplace = urlReplace.replace(/[^a-zA-Z0-9]+\s/g, "+");
+                let hashReplace = specialReplace.replace(/#/g, "+")
+                console.log(hashReplace);
+                let params = hashReplace.replace(/\s/g , "+");
+                
+                console.log(typeof entry)
+                let url = `http://www.datasciencetoolkit.org/text2sentiment/${params}`;
+                console.log(url);
+                
+                let trust = $sce.trustAsResourceUrl(url); 
+                return $http.jsonp(trust, {params : params}).then((rep) => {
+                    console.log(rep); 
+                });
+            }
+            for(let i = 0; i < response.data.text.length; i++){
+                loop(response.data.text[i]); 
+            }
            return vm.tweets = response;         
         })
         }
