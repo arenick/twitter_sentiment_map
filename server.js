736c8027424bf408a46a0fc1fc5856fc6b783bd0
Jonathon Branham <kefele@aim.com>
2018-09-11 14:52:04 -0500

    k

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static("./public"));
 app.use(express.json());
 app.use("/", twit);
 
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 9000;
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}.`);
