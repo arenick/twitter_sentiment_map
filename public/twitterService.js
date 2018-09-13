e90f69f0371087e51245047839adff2f7ff341a4
Kenneth <rekerc4@gmail.com>
2018-09-13 11:45:24 -0400

    coloring states on load.

--- public/twitterService.js
+++ public/twitterService.js
@@ -36,69 +36,77 @@ function TwitterService ($http, $sce) {
     }
 
     const states =  {
-        "Alabama": "288de3df481163e8", 
-        "Alaska": "07179f4fe0500a32",
-        "Arizona": "a612c69b44b2e5da",
-        "Arkansas": "e8ad2641c1cb666c",
-        "California": "fbd6d2f5a4e4a15e",
-        "Colorado": "e21c8e4914eef2b3",
-        "Connecticut": "e86b380cfefcced5",
-        "Delaware": "3f5897b87d2bf56c",
-        "Florida": "4ec01c9dbc693497",
-        "Georgia": "7142eb97ae21e839",
-        "Hawaii": "9dafd05b1158873b",
-        "Idaho": "4723507d8ce23a60",
-        "Illinois": "f54a2170ff4b15f7",
-        "Indiana": "1010ecfa7d3a40f8",
-        "Iowa": "3cd4c18d3615bbc9",
-        "Kansas": "27c45d804c777999",
-        "Kentucky": "6ffcf3b0b904bbcb",
-        "Louisiana": "1c73ebb264e145ee",
-        "Maine": "463f5d9615d7d1be",
-        "Maryland": "dea1eac2d7ef8878",
-        "Massachusetts": "cd450c94084cbf9b",
-        "Michigan": "67d92742f1ebf307",
-        "Minnesota": "9807c5c5f7a2c6ce",
-        "Mississippi": "43d2418301bf1a49",
-        "Missouri": "2526edd24c06e60c",
-        "Montana": "d2ddff69682ae534",
-        "Nebraska": "ac9b9070f6d17a9a",
-        "Nevada": "d374fb61a20fb74f",
-        "New Hampshire": "226b21641df42460",
-        "New Jersey": "65b4760a2b411e11", 
-        "New Mexico": "71d65c0e6d94efab",
-        "New York": "27485069891a7938",
-        "North Carolina": "3b98b02fba3f9753",
-        "North Dakota": "7d893ca2441b0c21",
-        "Ohio": "de599025180e2ee7",
-        "Oklahoma": "bd3d2074a33fbd06",
-        "Oregon": "df7fd3a3b9eff7ee",
-        "Pennsylvania": "dd9c503d6c35364b",
-        "Rhode Island": "6d50765616ee2e60",
-        "South Carolina": "6057f1e35bcc6c20",
-        "South Dakota": "d06e595eb3733f42",
-        "Tennessee": "7f7d58e5229c6b6c",
-        "Texas": "e0060cda70f5f341",
-        "Utah": "1879ace9e02ace61",
-        "Vermont": "9aa25269f04766ab",
-        "Virginia": "5635c19c2b5078d1",
-        "Washingston": "bc3a38d3d5999b4b",
-        "West Virginia": "2d83c71ce16cd187",
-        "Wisconsin": "7dc5c6d3bfb10ccc",
-        "Wyoming": "5669366953047e51"
+        "AL": "288de3df481163e8", 
+        "AK": "07179f4fe0500a32",
+        "AZ": "a612c69b44b2e5da",
+        "AR": "e8ad2641c1cb666c",
+        "CA": "fbd6d2f5a4e4a15e",
+        "CO": "e21c8e4914eef2b3",
+        "CT": "e86b380cfefcced5",
+        "DE": "3f5897b87d2bf56c",
+        "FL": "4ec01c9dbc693497",
+        "GA": "7142eb97ae21e839",
+        "HI": "9dafd05b1158873b",
+        "ID": "4723507d8ce23a60",
+        "IL": "f54a2170ff4b15f7",
+        "IN": "1010ecfa7d3a40f8",
+        "IA": "3cd4c18d3615bbc9",
+        "KS": "27c45d804c777999",
+        "KY": "6ffcf3b0b904bbcb",
+        "LA": "1c73ebb264e145ee",
+        "ME": "463f5d9615d7d1be",
+        "MD": "dea1eac2d7ef8878",
+        "MA": "cd450c94084cbf9b",
+        "MI": "67d92742f1ebf307",
+        "MN": "9807c5c5f7a2c6ce",
+        "MS": "43d2418301bf1a49",
+        "MO": "2526edd24c06e60c",
+        "MT": "d2ddff69682ae534",
+        "NE": "ac9b9070f6d17a9a",
+        "NV": "d374fb61a20fb74f",
+        "NH": "226b21641df42460",
+        "NJ": "65b4760a2b411e11", 
+        "NM": "71d65c0e6d94efab",
+        "NY": "27485069891a7938",
+        "NC": "3b98b02fba3f9753",
+        "ND": "7d893ca2441b0c21",
+        "OH": "de599025180e2ee7",
+        "OK": "bd3d2074a33fbd06",
+        "OR": "df7fd3a3b9eff7ee",
+        "PA": "dd9c503d6c35364b",
+        "RI": "6d50765616ee2e60",
+        "SC": "6057f1e35bcc6c20",
+        "SD": "d06e595eb3733f42",
+        "TN": "7f7d58e5229c6b6c",
+        "TX": "e0060cda70f5f341",
+        "UT": "1879ace9e02ace61",
+        "VT": "9aa25269f04766ab",
+        "VA": "5635c19c2b5078d1",
+        "WA": "bc3a38d3d5999b4b",
+        "WV": "2d83c71ce16cd187",
+        "WI": "7dc5c6d3bfb10ccc",
+        "WY": "5669366953047e51"
     }
 
     const smallStates = {
-        "Alabama": "288de3df481163e8", 
-        "Michigan": "67d92742f1ebf307",
-        "Arizona": "a612c69b44b2e5da",
-        "Arkansas": "e8ad2641c1cb666c",
-        "California": "fbd6d2f5a4e4a15e"
+        "AL": "288de3df481163e8", 
+        "MI": "67d92742f1ebf307",
+        "AZ": "a612c69b44b2e5da",
+        "AR": "e8ad2641c1cb666c",
+        "WA": "bc3a38d3d5999b4b",
+        "CA": "fbd6d2f5a4e4a15e"
     }
     
 
     vm.getAllTweets = () => {
-
+        
+        let processT2S = () => {
+            // ```this.setDataForState = (state, colorLevel, populationData) => {
+            //     simplemaps_usmap_mapdata.state_specific[state].color = colorLevel;
+            //     simplemaps_usmap_mapdata.state_specific[state].description = `Population: ${populationData}`;
+            // } 
+        }
+        
         let textSentimentApi = (usState, stateName) => {  
             return $http({
             method: "POST",
@@ -109,8 +117,8 @@ function TwitterService ($http, $sce) {
             data: {'test': stateName}
          }).then((response) => {
              console.log(response.data);
-             let data = {}; 
-             let sentimentArray = [];
+             
+             let sentimentCollector = [response.data.stateName];
              let loop = (entry) => {
                  //console.log(entry);
                  let urlReplace = entry.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
@@ -122,24 +130,74 @@ function TwitterService ($http, $sce) {
                  
                  let trust = $sce.trustAsResourceUrl(url); 
                  return $http.jsonp(trust, {params : params}).then((rep) => {
-                    // console.log(rep); 
+                    //console.log(rep); 
+                    sentimentCollector.push(rep.data.score);
+                    //console.log(sentimentCollector);
+                    return sentimentCollector;
+                
                  });
              }
+             let averageArr = [];
+             
+             let promise = new Promise((resolve, reject) => {
              for(let i = 0; i < response.data.text.length; i++){
-                 loop(response.data.text[i]); 
-                 
+               averageArr = loop(response.data.text[i]);  
              }
+             //console.log(averageArr); 
+             resolve(averageArr);  
+             }).then((ret) => {
+                 console.log(ret); 
+                //  console.log(ret.length); 
+                 let avgCol = 0
+                 let i = 1
+                 let notZero = 0
+
+                 for(i; i < ret.length; i++){
+                    //  console.log(i + "   i");
+                    //  console.log(ret[i] + "  ret[i]");
+                    //  console.log(avgCol + "   avgCol"); 
+                     if(ret[i] === 0){
+
+                     }
+                     else{
+                         avgCol  += ret[i];
+                         notZero++
+                        }
+                     
+                 }
+                 console.log(avgCol);
+                 console.log(notZero); 
+                 let avg = avgCol / notZero; 
+                 console.log(avg + "   average"); 
+                 if(avg > 0){
+                    let color = "rgba(82, 176, 93, 1)";  
+                    console.log(simplemaps_usmap_mapdata);
+                    simplemaps_usmap_mapdata.state_specific[ret[0]].color = color; 
+                    simplemaps_usmap.refresh(); 
+                }
+                 else if(avg < 0){
+                    let color = "rgba(255, 51, 81, 1)";
+                    simplemaps_usmap_mapdata.state_specific[ret[0]].color = color; 
+                    simplemaps_usmap.refresh(); 
+                 }
+                 else if(avg === 0){
+                     let color = "rgba(255, 210, 27, 1)";
+                     simplemaps_usmap_mapdata.state_specific[ret[0]].color = color; 
+                     simplemaps_usmap.refresh(); 
+                 }
+             });
+
+            // console.log(promise);
             return vm.tweets = response;                
          });
         }
         
-        let smallStateKeys = Object.keys(smallStates);
+        let smallStateKeys = Object.keys(smallStates);//for testing 
         let stateKeys = Object.keys(states); 
-        
+        console.log(smallStateKeys.length);
         for(let i = 0; i < smallStateKeys.length; i++){
-            // let state = states.stateKeys[i]; 
-           
-            textSentimentApi(states[smallStateKeys[i]], smallStateKeys[i]); 
+            //let state = states.stateKeys[i]; 
+           textSentimentApi(smallStates[smallStateKeys[i]], smallStateKeys[i]); 
         }
 
         
@@ -162,69 +220,71 @@ function TwitterService ($http, $sce) {
 
 
     
-    vm.getMichigan = ($scope) => {
-        return $http({
-           method: "GET",
-           url: "/state", 
-        }).then((response) => {
-            let stateEmotion = []; 
-            let stateEm = null; 
-            let sentimentArray = [];
-            let loop = (entry) => {
-                return $http({
-                    // "async": true,
-                    // "crossDomain": true,
-                    url: "https://apis.paralleldots.com/v3/emotion",
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    },
-                    data: {
-                        'text': entry,
-                        'api_key': 'DZEgFpGj5tlOGYaIYZ2hQAjDy2ARxY98tLs2Gsepptw'
-                    },
-                    transformRequest: deStringify
-                    }).then((response) => {
-                stateEmotion.push(response.data.emotion);
-                //console.log(stateEmotion);
-                let counts = {};
-                let compare = 0;
-                let mostFrequent = 0;
-                //console.log(stateEmotion.length);
-                for(let i = 0, len = stateEmotion.length; i < len; i++){
-                    let word = stateEmotion[i];
-                    if (counts[word] === undefined){
-                        counts[word] = 1;
-                    } else {
-                        counts[word] = counts[word] + 1;
-                    }
-                    if (counts[word] > compare){
-                        compare = counts[word];
-                        mostFrequent =  stateEmotion[i];
-                    }
-            }
-            //console.log(stateEmotion);
-            //console.log(mostFrequent);
-            return mostFrequent;
+    // vm.getMichigan = ($scope) => {
+    //     return $http({
+    //        method: "GET",
+    //        url: "/state", 
+    //     }).then((response) => {
+    //         let stateEmotion = []; 
+    //         let stateEm = null; 
+    //         let sentimentArray = [];
+    //         let loop = (entry) => {
+    //             return $http({
+    //                 // "async": true,
+    //                 // "crossDomain": true,
+    //                 url: //"https://apis.paralleldots.com/v3/emotion",
+    //                 method: "POST",
+    //                 headers: {
+    //                     "Content-Type": "application/x-www-form-urlencoded"
+    //                 },
+    //                 data: {
+    //                     'text': entry,
+    //                     'api_key': 'DZEgFpGj5tlOGYaIYZ2hQAjDy2ARxY98tLs2Gsepptw'
+    //                 },
+    //                 transformRequest: deStringify
+    //                 }).then((response) => {
+    //             stateEmotion.push(response.data.emotion);
+    //             //console.log(stateEmotion);
+    //             let counts = {};
+    //             let compare = 0;
+    //             let mostFrequent = 0;
+    //             //console.log(stateEmotion.length);
+    //             for(let i = 0, len = stateEmotion.length; i < len; i++){
+    //                 let word = stateEmotion[i];
+    //                 if (counts[word] === undefined){
+    //                     counts[word] = 1;
+    //                 } else {
+    //                     counts[word] = counts[word] + 1;
+    //                 }
+    //                 if (counts[word] > compare){
+    //                     compare = counts[word];
+    //                     mostFrequent =  stateEmotion[i];
+    //                 }
+    //         }
+    //         //console.log(stateEmotion);
+    //         //console.log(mostFrequent);
+    //         return mostFrequent;
 
-                return stateEmotion;
-                }).catch((error) => {
-                    //console.log(error);
-                });
-            }
+    //             return stateEmotion;
+    //             }).catch((error) => {
+    //                 //console.log(error);
+    //                 throw error;
+    //             });
+    //         }
 
-            for(let i = 0; i < response.data.text.length; i++){
-            let stateEm =  loop(response.data.text[i]); 
-            //console.log(stateEm); 
-            }
-            return stateEmotion;
+    //         for(let i = 0; i < response.data.text.length; i++){
+    //         let stateEm =  loop(response.data.text[i]); 
+    //         //console.log(stateEm); 
+    //         }
+    //         return stateEmotion;
                        
-           }).then((ret) => {
-           // console.log(ret); 
-           }).catch((error) => { 
-             //  console.log(error);
-           });  
-        }
+    //        }).then((ret) => {
+    //        // console.log(ret); 
+    //        }).catch((error) => { 
+    //          //  console.log(error);
+    //          throw error; 
+    //        });  
+    //     }
        
 
 
