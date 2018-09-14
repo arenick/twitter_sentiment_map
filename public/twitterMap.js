78c513ac97349e9023ccc27b0a71c2acef9cf61e
Kenneth <rekerc4@gmail.com>
2018-09-14 14:27:36 -0400

    Workign code for new master pull

--- public/twitterMap.js
+++ public/twitterMap.js
@@ -25,7 +25,25 @@ const twitterMap = {
             // simplemaps_usmap_mapdata.state_specific[stateName].color = "yellow";
             simplemaps_usmap.refresh();
             TwitterService.getState(stateName).then((response) => {
-                $timeout([console.log(response)], [10000]);
+                let f = function(){
+                    console.log(response.emotion[0].emotion)
+                //     switch(response.emotion[0].emotion){
+                //         case 'Angry': 
+                //             console.log("switch angry"); 
+                //             simplemaps_usmap_mapdata.state_specific[stateName].color = '#ff3351'; 
+                //         case 'Bored': 
+                //             simplemaps_usmap_mapdata.state_specific[stateName].color = '#52b05d';
+                //         case 'Excited': 
+                //             simplemaps_usmap_mapdata.state_specific[stateName].color = '#ff951a';
+                //         case 'Fear': 
+                //             simplemaps_usmap_mapdata.state_specific[stateName].color = '#52e0bb';
+                //         case 'Happy': 
+                //             simplemaps_usmap_mapdata.state_specific[stateName].color = '#ffd21b';
+                //         case 'Sad': 
+                //             simplemaps_usmap_mapdata.state_specific[stateName].color = '#ffd21b';
+                //     }
+                }
+                $timeout(f, 1000);
                 vm.tweetStuff = response.text;
             });
         });
