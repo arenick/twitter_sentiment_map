8dcdca154b2c5a95310b529ef0364821962f53b6
John Permaloff <jvpermaloff@gmail.com>
2018-09-14 15:16:11 -0400

    lol more

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const twit = require("./routes/routes");
-
+console.log("yay");
 app.use(express.static("./public"));
 app.use(express.json());
 app.use("/", twit);
