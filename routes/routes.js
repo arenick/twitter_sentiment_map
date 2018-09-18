2e4080ad6d1ffb88f4d409764983a0f6050062ff
John Permaloff <jvpermaloff@gmail.com>
2018-09-18 13:56:38 -0400

    test

--- routes/routes.js
+++ routes/routes.js
@@ -10,10 +10,10 @@ const emotion = require('paralleldots/apis/emotion');
 var Twit = require("twit");
 
   var T = new Twit({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
-    access_token_secret: process.env.access_token_secret,
+    consumer_key: consumer_key,
+    consumer_secret: consumer_secret,
+    access_token: access_token,
+    access_token_secret: access_token_secret,
     strictSSL: true,
  });
 
